eb513ed499d43ff07215e6e431e3aad65ce9ad99
Muhammad <159750858+MuhAhmad312@users.noreply.github.com>
2025-11-13 13:41:11 +0500

    testing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask 
-import os 
-app = Flask(__name__) 
+from flask import Flask
+import os
+app = Flask(__name__)
 @app.route("/")
-def hello(): 
-    return f"Hello World, pod id {os.environ.get('HOSTNAME', 'unknown')}!" 
-if __name__ == "__main__": 
+def hello():
+    return f"Hello World, pod id {os.environ.get('HOSTNAME', 'unknown')}!"
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
